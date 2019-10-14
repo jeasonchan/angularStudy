d0f61e9737bcddd1e20f97312a210a9df0f2a42d
jeasonchan <439994526@qq.com>
2019-10-14 23:51:02 +0800

    开始导入angular material组件

--- angular哔哩哔哩教程实践/Demo1/src/app/app.module.ts
+++ angular哔哩哔哩教程实践/Demo1/src/app/app.module.ts
@@ -8,18 +8,27 @@ import { AppComponent } from './app.component';
 import { Test1Component } from './components/test1/test1.component';
 import { Test2Component } from './components/test2/test2.component';
 import { Test3Component } from './components/test3/test3.component';
+import { Test4Component } from './components/test4/test4.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// angular material 模块，一般情况下，可以再用个module包装一下
+import { MatButtonModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     Test1Component,
     Test2Component,
-    Test3Component
+    Test3Component,
+    Test4Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
