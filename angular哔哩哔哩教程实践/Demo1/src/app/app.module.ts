2d7a2b8b0e823bdbda19f23acfa63632060baf49
jeasonchan <439994526@qq.com>
2019-10-16 23:56:47 +0800

    陈成功安装materialå¹，引入模块，实现menu按钮

--- angular哔哩哔哩教程实践/Demo1/src/app/app.module.ts
+++ angular哔哩哔哩教程实践/Demo1/src/app/app.module.ts
@@ -8,18 +8,25 @@ import { AppComponent } from './app.component';
 import { Test1Component } from './components/test1/test1.component';
 import { Test2Component } from './components/test2/test2.component';
 import { Test3Component } from './components/test3/test3.component';
+import { TestComponent } from './components/test/test.component';
+import {MatMenuModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TestComponent,
     Test1Component,
     Test2Component,
-    Test3Component
+    Test3Component,
+    TestComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular哔哩哔哩教程实践/Demo1/src/app/components/test/test.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatMenuTrigger} from '@angular/material';
+
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
